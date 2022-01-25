2aa32dcbd0e1fcb6aa2c3263e301dc1c8854e612
Trivenianumolu <trivenianumolu99@gmail.com>
2022-01-25 15:53:28 +0530

    First Submission

---
+++ Hello5times/Hello5times/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello5times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1;i<=5;i++)
+                Console.WriteLine("Hello");
+            Console.ReadLine();
+        }
+    }
+}
