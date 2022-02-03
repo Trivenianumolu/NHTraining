ef8d76ac7c620ade9efd2999cc9507b43ca73983
Trivenianumolu <trivenianumolu99@gmail.com>
2022-02-03 11:52:30 +0530

    Day8 Submission

---
+++ Day8MorningAssignment/Day8Project2/Day8Project2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8Project2
+{
+    class Employee
+    {
+        public int id;
+        public string name;
+        public int salary;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+               new Employee(){id=1,name="abc" ,salary=30000},
+               new Employee(){id=2,name="bcd" ,salary=12000},
+               new Employee(){id=3,name="cde" ,salary=31200},
+               new Employee(){id=4,name="def" ,salary=45330},
+               new Employee(){id=5,name="efg" ,salary=20000}
+
+            };
+            //for loop
+            for(int i=0;i<employees.Count;i++)
+            {
+                Console.WriteLine($"id={employees[i].id}, name={employees[i].name}, salary={employees[i].salary}");
+            }
+            
+            //foreach loop
+            foreach(var e in employees)
+                Console.WriteLine($"id={e.id}, name={e.name}, salary={e.salary}");
+            Console.ReadLine();
+            //lambda expression
+            employees.ToList().ForEach(e => Console.WriteLine($"id={e.id}, name={e.name}, salary={e.salary}"));
+            //LINQ 
+            var result = from e in employees
+                         select e;
+            result.ToList().ForEach(e => Console.WriteLine($"id={e.id}, name={e.name}, salary={e.salary}"));        }
+    }
+}
---
+++ Day8MorningAssignment/Day8Project5/Day8Project5/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8Project5
+{
+   
+        
+            //Author: Triveni Anumolu
+            //Program: Own class with own variables and calling for,foreach,Lamda,LINQ
+     class Office
+     {
+            public string name;
+            public string location;
+            public string type;
+     }
+     class Program
+     {
+            static void Main(string[] args)
+            {
+                List<Office> off = new List<Office>()
+                {
+                    new Office() { name = "nbhealthcare", location = "Madhapur", type = "IT" },
+                    new Office() { name = "eenadu", location = "Irrummanzil", type = "Paper" },
+                    new Office() { name = "byjus", location = "jubliee hills", type = "education" },
+                    new Office() { name = "sutherland", location = "Lanco hills", type = "IT" },
+                    new Office() { name = "accenture", location = "Mindspace", type = "IT" }
+                };
+            //for loop
+            for (int i = 0; i < off.Count; i++)
+            {
+                Console.WriteLine($"name={off[i].name}, location={off[i].location}, type={off[i].type}");
+            }
+
+            //foreach loop
+            foreach (var o in off)
+            {
+                Console.WriteLine($"name={o.name}, location={o.location}, type={o.type}");
+            }
+
+            //Lamda Expression
+
+            off.ToList().ForEach(o => Console.WriteLine($"name={o.name}, location={o.location}, type={o.type}"));
+
+            //LINQ Query
+            var result = from o in off
+                         select o;
+            result.ToList().ForEach(o => Console.WriteLine($"name={o.name}, location={o.location}, type={o.type}"));
+
+            Console.ReadLine();
+
+
+
+            }
+     }
+}
+
+
+
+            
