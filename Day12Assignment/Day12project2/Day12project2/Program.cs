95a997d78366578ee80feb47ae592d561b56af22
Trivenianumolu <trivenianumolu99@gmail.com>
2022-02-08 18:18:00 +0530

    Day12

---
+++ Day12Assignment/Day12project2/Day12project2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a, b, c;
+                Console.WriteLine("Enter a value");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter b value");
+                b = Convert.ToInt32(Console.ReadLine());
+                c = a / b;
+                Console.WriteLine(c);
+                
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Denomintor shouldn't be zero. Enter a valid input");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Format is not correct.Enter a numerical value");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Given input is out of range. Enter a value between 1 to 1000000");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Some error occured. Contact@abc.gmail.com");
+            }
+
+            finally
+            {
+                Console.WriteLine("\n\n\n\n\nDesigned by abc");
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
