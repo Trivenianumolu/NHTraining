9cb26ac1674f97b55073291781bb08b58dd39798
Trivenianumolu <trivenianumolu99@gmail.com>
2022-02-15 16:32:26 +0530

    Day 17

---
+++ Day17Assignment/TriveniAnumoluLibrary/PublicLibrary/PublicBaseClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriveniAnumoluLibrary;
+
+namespace PublicLibrary
+{
+    public class PublicBaseClass:BaseClass
+    {
+        
+            public void ReadClass()
+            {
+                a = 1;
+                b = 2;
+                c = 3;
+                d = 4;
+                e = 5;
+            }
+        
+    }
+    public class Other
+    {
+
+        public void ReadData()
+        {
+            BaseClass bc = new BaseClass();
+            bc.a = 1;
+            bc.b = 2;
+            bc.c = 3;
+            bc.d = 4;
+            bc.e = 5;
+        }
+
+
+       
+    }
+}
---
+++ Day17Assignment/TriveniAnumoluLibrary/TriveniAnumoluLibrary/TriveniBaseClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriveniAnumoluLibrary
+{
+    public class BaseClass
+    {
+        public int a;
+        private int b;
+        protected int c;
+        internal int d;
+        protected internal int e;
+        public  void ReadBaseClass()
+        {
+            a = 1;
+            b = 2;
+            c = 3;
+            d = 4;
+            e = 5;   
+        }
+    }
+    public class DerivedClass:BaseClass
+    {
+        public void ReadDerivedClass()
+        {
+            a = 1;
+            b = 2;
+            c = 3;
+            d = 4;
+            e = 5;
+
+
+        }
+    }
+    public class OtherClass
+    {
+        public void ReadOtherClass()
+        {
+            BaseClass bc = new BaseClass();
+            bc.a = 1;
+            bc.b = 2;
+            bc.c = 3;
+            bc.d = 4;
+            bc.e = 5;
+        }
+    }
+
+
+}
