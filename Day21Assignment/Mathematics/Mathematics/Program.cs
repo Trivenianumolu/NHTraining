aa34e533d09d2b42053b28503a70cec2e655aa5f
Trivenianumolu <trivenianumolu99@gmail.com>
2022-02-21 12:51:04 +0530

    Day 21

---
+++ Day21Assignment/Mathematics/Mathematics/Program.cs
@@ -0,0 +1,22 @@
+﻿using Mathematics.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebService1SoapClient web = new WebService1SoapClient();
+            Console.WriteLine(web.Add(2,3));
+            Console.WriteLine(web.Mul(3,4));
+            Console.WriteLine(web.Div(5,5));
+            Console.WriteLine(web.Factorial(6));  
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ Day21Assignment/WindowsForms/WindowsForms/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsForms.ServiceReference1;
+
+namespace WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text=Convert.ToString(textBox1.Text);
+            WebService1SoapClient web = new WebService1SoapClient();
+            textBox2.Text = web.WebService1.Factorial(5);
+
+
+        }
+    }
+}
